cbb631bc9ab9fab0d2f95f717d2793fab8c3fb5d
Sameer <sameersitre@gmail.com>
2018-09-25 18:40:44 +0530

    3D roll ball from arrow keys

---
+++ RollaBall/Assets/Scripts/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public GameObject Player;
+    private Vector3 Offset;
+	// Use this for initialization
+	void Start () {
+        Offset = transform.position - Player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.position = Player.transform.position + Offset;
+	}
+}
---
+++ RollaBall/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class PlayerController : MonoBehaviour {
+    private Rigidbody rb;
+    public float speed;
+    private float count;
+    public Text CountText;
+    public Text winText;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        setcountText();
+        
+    }
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+        //movement.add
+        //cs
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Pick Up")){
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            setcountText();
+        }
+    }
+
+    void setcountText(){
+        CountText.text = "Score : " + count.ToString();
+        if(count >= 11){
+            winText.text = "You Win!!!";
+        }
+    }
+}
